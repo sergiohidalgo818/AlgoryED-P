ed5ac7329b647b2eda4e7d5fabb82649ad737321
Sergio <sergiohg818@gmail.com>
2023-09-18 18:48:31 +0200

    faltan bb y recbb y saber el nombre de las variables

--- p1NN/p1NN.py
+++ p1NN/p1NN.py
@@ -1,6 +1,7 @@
 import numpy as np
 from typing import Tuple
 import timeit
+from typing import List
 
 def matrix_multiplication(m_1: np.ndarray, m_2: np.ndarray)-> np.ndarray:
 
@@ -18,6 +19,13 @@ def matrix_multiplication(m_1: np.ndarray, m_2: np.ndarray)-> np.ndarray:
     return m_t
 
 
+def rec_bb(t: List, f: int, l: int, key: int)-> int:
+    pass
+
+def bb(t: List, f: int, l: int, key: int)-> int:
+    pass
+
+
 l_timings = []
 for i in range(10, 21):
     dim = 10+i**2
@@ -25,4 +33,25 @@ for i in range(10, 21):
     timings = %timeit -o -n 10 -r 5 -q matrix_multiplication(m, m)
     l_timings.append([dim, timings.best])
 
-print(l_timings)
+print("\nTimings arrays " +l_timings)
+
+
+l_timingsbb = []
+for size in enumerate(range(5, 15)):
+    t = list(range(2**i * size))
+    key = ...
+    timings = %timeit -n 100 -r 10 -o -q rec_bb(t, 0, len(t) - 1, key)
+    l_timingsbb.append([len(t), timings.best])
+    a_timings = np.array(a_timings)
+
+print("\nTimings bb " + l_timingsbb)
+
+l_timingsbbi = []
+for size in enumerate(range(5, 15)):
+    t = list(range(2**i * size))
+    key = ...
+    timings = %timeit -n 100 -r 10 -o -q bb(t, 0, len(t) - 1, key)
+    l_timingsbbi.append([len(t), timings.best])
+    a_timingsbbi = np.array(l_timingsbbi)
+
+print("\nTimings bb " + a_timingsbbi)
