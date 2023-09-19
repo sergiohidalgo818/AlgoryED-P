1e7534bfd1dad0038abdbafc3697bcabe57cc0eb
Sergio <sergiohg818@gmail.com>
2023-09-19 16:14:10 +0200

    problema sol, solo queda ver lo del size y preguntar si se tiene que copiar el output en 1 fichero

--- p1NN/p1NN.py
+++ p1NN/p1NN.py
@@ -1,6 +1,5 @@
 import numpy as np
 from typing import Tuple
-import timeit
 from typing import List
 
 # 1A
@@ -9,7 +8,7 @@ def matrix_multiplication(m_1: np.ndarray, m_2: np.ndarray)-> np.ndarray:
     # Se obtienen las dimensiones de cada matriz
     shape_m_1 = np.shape(m_1)
     shape_m_2 = np.shape(m_2)
-
+    print()
     # Si son incompatibles, devuelve None
     if shape_m_1[1] != shape_m_2[0]:
         return None
@@ -17,42 +16,25 @@ def matrix_multiplication(m_1: np.ndarray, m_2: np.ndarray)-> np.ndarray:
     # la matriz resultante tendra las filas de m1 y columnas de m2
     m_r: np.ndarray = np.empty ((shape_m_1[0],shape_m_2[1])) 
     # Si son compatibles, se multiplican y se devuelve la matriz resultante 
-    # con np.dot :
-    # m_r: np.ndarray = m_1.dot(m_2)
 
-    # m_1 -> (i,j) m_2 -> (k,z)
+    # m_1 -> (i,x) m_2 -> (x,z)
     # bucle de filas m_1
     for i in range(shape_m_1[0]):
         # bucle de columnas m_2
-        for z in range(shape_m_2[1]):
+        for j in range(shape_m_2[1]):
             num = 0
             # bucle de sumas y multiplicaciones
-            for k,j in enumerate(range(shape_m_1[1])):
+            for x in range(shape_m_1[1]):
                 # se multiplica cada numero correspondiente 
                 # de la fila de m_1 y la columna de m_2
-                num += m_1[i,j]*m_2[k,z]
+                num += m_1[i,x]*m_2[x,j]
 
             # se multimplican ambos valores
-            m_r[i,z] = num
-
+            m_r[i,j] = num
+            
     return m_r
 
-# lista de tiempos
-l_timings = []
-for i in range(10, 21):
-    # dimension de la matriz
-    dim = 10+i**2
-    # crea una matriz de dimension dimXdim con valores 
-    # aleatorios entre 0 y 1 
-    m = np.random.uniform(low=0, high=1, size=(dim, dim)) 
-    # guarda con el comando "magico" %timeit los valores de 
-    # tiempo que tarda en realizar la funcion 
-    # con 5 repeticiones de 10 iteraciones cada una
-    timings = %timeit -o -n 10 -r 5 -q matrix_multiplication(m, m)
-    # se guarda en la lista
-    l_timings.append([dim, timings.best])
 
-print("\nTimings arrays " +str(l_timings))
 
 #1B
 def rec_bb(t: List, f: int, l: int, key: int)-> int:
@@ -95,28 +77,6 @@ def bb(t: List, f: int, l: int, key: int)-> int:
         
         # si no se ha devuelto se calcula de nuevo el mediano
         m = int((f+l)/2)
-
-
-l_timingsbb = []
-for size in enumerate(range(5, 15)):
-    t = list(range(2**i * size[1]))
-    key = size[0]
-    timings = %timeit -n 100 -r 10 -o -q rec_bb(t, 0, len(t) - 1, key)
-    l_timingsbb.append([len(t), timings.best])
-    a_timings = np.array(l_timingsbb)
-
-print("\nTimings bbrec " + str(a_timings))
-
-l_timingsbbi = []
-for size in enumerate(range(5, 15)):
-    t = list(range(2**i * size[1]))
-    key = size[0]
-    timings = %timeit -n 100 -r 10 -o -q bb(t, 0, len(t) - 1, key)
-    l_timingsbbi.append([len(t), timings.best])
-    a_timingsbbi = np.array(l_timingsbbi)
-
-print("\nTimings bbi " + str(a_timingsbbi))
-
 
 #1C
 def matrix_multiplication_dot(m_1: np.ndarray, m_2: np.ndarray)-> np.ndarray:
---
+++ p1NN/test.py
@@ -0,0 +1,66 @@
+import numpy as np
+from typing import Tuple
+
+if __name__=='__main__':
+
+    # m_1: np.ndarray = np.empty((3,3))
+    # m_2: np.ndarray = np.empty((3,3))
+
+    # shape_m_1 = np.shape(m_1)
+    # shape_m_2 = np.shape(m_2)
+
+    # print("m1:\n")
+    # for i in range(shape_m_1[0]):
+    #     for j in range(shape_m_1[1]):
+    #         m_1[i][j] = float(input('Elemento (%d,%d): ' % (i, j)))
+ 
+    # print("\nm2:\n")
+
+    # for i in range(shape_m_2[0]):
+    #     for j in range(shape_m_2[1]):
+    #         m_2[i][j] = float(input('Elemento (%d,%d): ' % (i, j)))
+
+
+    # m_r: np.ndarray = np.empty ((shape_m_1[0],shape_m_2[1])) 
+
+
+    # print("\nm1\n" + str(m_1))
+    # print("\nm2\n" + str(m_2))
+
+    # # Si son incompatibles, devuelve None
+    # if shape_m_1[1] != shape_m_2[0]:
+    #     print("No se pueden mult")
+    #     exit()
+
+    # m_rauto: np.ndarray = m_1.dot(m_2)
+
+    for i in range(10, 21):
+        # dimension de la matriz
+        dim = 10+i**2
+        # crea una matriz de dimension dimXdim con valores 
+        # aleatorios entre 0 y 1 
+        m = np.random.uniform(low=0, high=1, size=(dim, dim)) 
+
+        m_1: np.ndarray = m
+        m_2: np.ndarray = m
+
+        shape_m_1 = np.shape(m_1)
+        shape_m_2 = np.shape(m_2)
+        m_r: np.ndarray = np.empty ((shape_m_1[0],shape_m_2[1])) 
+
+        for i in range(shape_m_1[0]):
+            # bucle de columnas m_2
+            for j in range(shape_m_2[1]):
+                num = 0
+                # bucle de sumas y multiplicaciones
+                for x in range(shape_m_1[1]):
+                    # se multiplica cada numero correspondiente 
+                    # de la fila de m_1 y la columna de m_2
+                    num += m_1[i,x]*m_2[x,j]
+
+                # se multimplican ambos valores
+                m_r[i,j] = num
+            m_rauto: np.ndarray = m_1.dot(m_2)
+
+            print("fun = " +str(m_r))
+            print("auto = " +str(m_rauto))
