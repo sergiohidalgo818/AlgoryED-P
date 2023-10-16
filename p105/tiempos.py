da18b7001121d241a95438222ebd7297f23c9793
Sergio <sergiohg818@gmail.com>
2023-10-16 15:34:08 +0200

    cuestiones y ejecutar

--- p105/tiempos.py
+++ p105/tiempos.py
@@ -15,7 +15,7 @@ def f(N):
 def tofit(x, a, b):
     return a * f(x) + b
 
-def mishuevos():
+def graficasDot():
     # Supongamos que tienes un array de tiempos reales a_timings
     with open('./data/array.exe_data.json', 'r') as file:
         data = json.load(file)
@@ -54,7 +54,7 @@ def mishuevos():
     plt.savefig('ej1.png')
     plt.close()
 
-def generar_graficas():
+def graficasMult():
     # Cargar los datos desde el archivo JSON
     with open('./data/array.exe_data.json', 'r') as file:
         data = json.load(file)
@@ -92,6 +92,113 @@ def generar_graficas():
     plt.savefig('grafica_array_dot.png')
     plt.close()
 
+
+def graficasBB():
+    # Cargar los datos desde el archivo JSON
+    with open('./data/exe_data.json', 'r') as file:
+        data = json.load(file)
+
+    bb_iter = np.array(data['bb_iter'])
+    bb_rec = np.array(data['bb_rec'])
+
+    # Extraer los valores de N y los tiempos de ejecución
+    N_bbi, tiempos_bbi = bb_iter[:, 0], bb_iter[:, 1]
+    N_bbrec, tiempos_bbrec = bb_rec[:, 0], bb_rec[:, 1]
+
+    # Crear la primera gráfica para array_mult
+    plt.figure(figsize=(10, 5))
+    plt.plot(N_bbi, tiempos_bbi, marker='o', label='bbisearch')
+    plt.xlabel('N')
+    plt.ylabel('Tiempo de Ejecución (s)')
+    plt.title('Tiempo bb() ')
+    plt.legend()
+    plt.grid(True)
+
+    # Guardar la primera gráfica en un archivo PNG
+    plt.savefig('grafica_bbi.png')
+    plt.close()
+
+       # Crear la primera gráfica para array_mult
+    plt.figure(figsize=(10, 5))
+    plt.plot(N_bbrec, tiempos_bbrec, marker='o', label='bbrecsearch')
+    plt.xlabel('N')
+    plt.ylabel('Tiempo de Ejecución (s)')
+    plt.title('Tiempo bb_rec() ')
+    plt.legend()
+    plt.grid(True)
+
+    # Guardar la primera gráfica en un archivo PNG
+    plt.savefig('grafica_bb_rec.png')
+    plt.close()
+
+
+    # Crear la primera gráfica para array_mult
+    plt.figure(figsize=(10, 5))
+    plt.plot(N_bbi, tiempos_bbi, marker='o', label='bbisearch')
+    plt.plot(N_bbrec, tiempos_bbrec, marker='o', label='bbrecsearch')
+    plt.xlabel('N')
+    plt.ylabel('Tiempo de Ejecución (s)')
+    plt.title('Tiempo bbi() & bb_rec() ')
+    plt.legend()
+    plt.grid(True)
+
+    # Guardar la primera gráfica en un archivo PNG
+    plt.savefig('grafica_bbi_rec.png')
+    plt.close()
+
+
+
+def graficasCreateHeap():
+    # Cargar los datos desde el archivo JSON
+    with open('./data/exe_data.json', 'r') as file:
+        data = json.load(file)
+
+    timings_minh = np.array(data['l_timings_min'])
+
+    # Extraer los valores de N y los tiempos de ejecución
+    N_minh, tiempos_createminh = timings_minh[:, 0], timings_minh[:, 1]
+
+    # Crear la primera gráfica para array_mult
+    plt.figure(figsize=(10, 5))
+    plt.plot(N_minh, tiempos_createminh, marker='o', label='createMinheap')
+    plt.xlabel('N')
+    plt.ylabel('Tiempo de Ejecución (s)')
+    plt.title('Tiempo create_min_heap() ')
+    plt.legend()
+    plt.grid(True)
+
+    # Guardar la primera gráfica en un archivo PNG
+    plt.savefig('grafica_minheap.png')
+    plt.close()
+
+
+def graficasShortHeap():
+    # Cargar los datos desde el archivo JSON
+    with open('./data/exe_data.json', 'r') as file:
+        data = json.load(file)
+
+    l_timings_minsh = np.array(data['l_timings_minsh'])
+
+    # Extraer los valores de N y los tiempos de ejecución
+    N_minhsh, tiempos_minhsh = l_timings_minsh[:, 0], l_timings_minsh[:, 1]
+
+    # Crear la primera gráfica para array_mult
+    plt.figure(figsize=(10, 5))
+    plt.plot(N_minhsh, tiempos_minhsh, marker='o', label='shortMinheap')
+    plt.xlabel('N')
+    plt.ylabel('Tiempo de Ejecución (s)')
+    plt.title('Tiempo min_heap_sort() ')
+    plt.legend()
+    plt.grid(True)
+
+    # Guardar la primera gráfica en un archivo PNG
+    plt.savefig('grafica_shortheap.png')
+    plt.close()
+
+
 if __name__ == "__main__":
-    generar_graficas()
-    mishuevos()
+    graficasMult()
+    graficasDot()
+    graficasBB()
+    graficasCreateHeap()
+    graficasShortHeap()
