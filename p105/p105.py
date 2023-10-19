47f8050bf03279d1c9607bd1c4ee15eaf0840e34
sergio <sergiohg818@gmail.com>
2023-10-19 02:26:23 +0200

    faltan los datos

--- p105/p105.py
+++ p105/p105.py
@@ -3,7 +3,9 @@ from typing import Tuple
 from typing import List
 
 # I-A
-def matrix_multiplication(m_1: np.ndarray, m_2: np.ndarray)-> np.ndarray:
+
+
+def matrix_multiplication(m_1: np.ndarray, m_2: np.ndarray) -> np.ndarray:
 
     # Se obtienen las dimensiones de cada matriz
     shape_m_1 = np.shape(m_1)
@@ -11,10 +13,10 @@ def matrix_multiplication(m_1: np.ndarray, m_2: np.ndarray)-> np.ndarray:
     # Si son incompatibles, devuelve None
     if shape_m_1[1] != shape_m_2[0]:
         return None
-    
+
     # la matriz resultante tendra las filas de m1 y columnas de m2
-    m_r: np.ndarray = np.empty ((shape_m_1[0],shape_m_2[1])) 
-    # Si son compatibles, se multiplican y se devuelve la matriz resultante 
+    m_r: np.ndarray = np.empty((shape_m_1[0], shape_m_2[1]))
+    # Si son compatibles, se multiplican y se devuelve la matriz resultante
 
     # m_1 -> (i,x) m_2 -> (x,z)
     # bucle de filas m_1
@@ -24,68 +26,70 @@ def matrix_multiplication(m_1: np.ndarray, m_2: np.ndarray)-> np.ndarray:
             num = 0
             # bucle de sumas y multiplicaciones
             for x in range(shape_m_1[1]):
-                # se multiplica cada numero correspondiente 
+                # se multiplica cada numero correspondiente
                 # de la fila de m_1 y la columna de m_2
-                num += m_1[i,x]*m_2[x,j]
+                num += m_1[i, x]*m_2[x, j]
 
             # se multiplican ambos valores
-            m_r[i,j] = num
-            
+            m_r[i, j] = num
+
     return m_r
 
 
-
 # I-B
-def rec_bb(t: List, f: int, l: int, key: int)-> int:
+def rec_bb(t: List, first: int, last: int, key: int) -> int:
     # m es la parte media de la lista (primero + ultimo)/2
-        
-    m = int((f+l)/2)
+
+    m = int((first+last)/2)
     # si el elemento del medio es mayor que el numero a buscar
     if t[m] > key:
         # se cambia el ultimo por el mediano menos 1
         # y se llama de nuevo a la funcion
-        rec_bb(t, f, m-1, key)
+        rec_bb(t, first, m-1, key)
     # si el elemento del medio es menor que el numero a buscar
     elif t[m] < key:
         # se cambia el primero por el mediano mas 1
         # y se llama de nuevo a la funcion
-        rec_bb(t, m+1, l, key)
-    # si es igual se devuelve m 
+        rec_bb(t, m+1, last, key)
+    # si es igual se devuelve m
     elif t[m] == key:
         return m
-    
-    # si el primero y el segundo son iguales  
-    elif f==l:
+
+    # si el primero y el segundo son iguales
+    elif first == last:
         # returna None
         return None
 
-def bb(t: List, f: int, l: int, key: int)-> int:
+
+def bb(t: List, first: int, last: int, key: int) -> int:
     # m es la parte media de la lista (primero + ultimo)/2
-    m = int((f+l)/2)
+    m = int((first+last)/2)
 
     # mientras el elemento del medio sea distinto a la key
     while t[m] != key:
 
         # si el elemento del medio es mayor que el numero a buscar
         if t[m] > key:
-            #se cambia el ultimo por el mediano menos 1
-            l = m-1
+            # se cambia el ultimo por el mediano menos 1
+            last = m-1
         # si el elemento del medio es menor que el numero a buscar
         elif t[m] < key:
             # se cambia el primero por el mediano mas 1
-            f = m+1
+            first = m+1
         # si es igual se devuelve m
         elif t[m] == key:
             return m
         # si no se ha devuelto se calcula de nuevo el mediano
-        m = int((f+l)/2)
-    
-    # si el primero y el segundo son iguales  
-    if f==l:
+        m = int((first+last)/2)
+
+    # si el primero y el segundo son iguales
+    if first == last:
         return None
 
 # I-C
-def matrix_multiplication_dot(m_1: np.ndarray, m_2: np.ndarray)-> np.ndarray:
+
+
+def matrix_multiplication_dot(m_1: np.ndarray, m_2: np.ndarray) -> np.ndarray:
 
     # Se obtienen las dimensiones de cada matriz
     shape_m_1 = np.shape(m_1)
@@ -94,97 +98,153 @@ def matrix_multiplication_dot(m_1: np.ndarray, m_2: np.ndarray)-> np.ndarray:
     # Si son incompatibles, devuelve None
     if shape_m_1[1] != shape_m_2[0]:
         return None
-    
+
     # la matriz resultante tendra las filas de m1 y columnas de m2
-    m_r: np.ndarray = np.empty ((shape_m_1[0],shape_m_2[1])) 
-    # Si son compatibles, se multiplican y se devuelve la matriz resultante 
+    m_r: np.ndarray = np.empty((shape_m_1[0], shape_m_2[1]))
+    # Si son compatibles, se multiplican y se devuelve la matriz resultante
     # con np.dot :
     m_r: np.ndarray = m_1.dot(m_2)
 
     return m_r
 
 # II-A
+
+
 def min_heapify(h: np.ndarray, i: int):
     tam = len(h) - 1
+
+    # control de errores
     if i < 0 or i > tam:
         return None
 
-    l = 2 * i + 1
-    r = 2 * i + 2     
-   
-    while l <= tam:
-        largest = i 
-        if h[i] > h[l]:
-            largest = l
-        if r <= tam and h[i] > h[r] and h[r] < h[largest]:
-            largest = r
-        if largest > i:
-            h[i], h[largest] = h[largest], h[i]
-            i = largest
+    # dato hijo izquierdo
+    left = 2 * i + 1
+
+    # dato hijo derecho
+    right = 2 * i + 2
+
+    while left <= tam:
+        # se toma como minimo inicial el elemento
+        # en la posicion del indice de la funcion
+        minimum = i
+
+        # se comparan los elementos de la izquierda y
+        # el del indice introducido en la funcion
+        # si el introducido es mas grande que el izquierdo
+        if h[i] > h[left]:
+            # el izquierdo pasa a ser el minimo
+            minimum = left
+
+        # si el derecho es menor que el introducido y
+        # que el minimo o el derecho mayor o igual al tamaño
+        if right <= tam and h[i] > h[right] and h[right] < h[minimum]:
+            # el derecho pasa a ser el minimo
+            minimum = right
+
+        # en caso de que sea mayor que el introducido
+        if minimum > i:
+            # se hace swap entre el elemento introducido y el minimo
+            h[i], h[minimum] = h[minimum], h[i]
+            # y el introducido sera el minimo
+            i = minimum
+        # en otro caso para
         else:
             break
 
-    
 
-def insert_min_heap(h: np.ndarray, k: int)-> np.ndarray:
+def insert_min_heap(h: np.ndarray, k: int) -> np.ndarray:
+
+    # control de errores
     if h is None:
         return np.array([k])
-    
-    i = len(h) 
+
+    i = len(h)
+    # se añade al final de la array
     h = np.append(h, k)
 
+    # mientras la i se mayor que 0 y
+    # el padre es mayor que el elemento en i
     while i > 0 and h[(i-1)//2] > h[i]:
+        # se swapean el elemento i y su padre
         h[i], h[(i-1)//2] = h[(i-1)//2], h[i]
+        # y se continua la ejecuccion por el
         i = (i-1)//2
     return h
 
 
-
 def create_min_heap(h: np.ndarray):
+
+    # control de errores
     if h is None:
         return None
 
+    # bucle inverso desde el padre
     for node in range((len(h) - 1) // 2, -1, -1):
+        # se llama a la funcion min_heapyfi por cada iteracion
         min_heapify(h, node)
 
     return h
-            
-def minheap_extract(h:np.ndarray) -> Tuple[int, np.ndarray]:
+
+
+def min_heap_extract(h: np.ndarray) -> Tuple[int, np.ndarray]:
+
     tam = len(h)
+
+    # control de errores
     if tam == 0:
         return list()
-    
+
+    # el elemento raiz sera el primero
     root = h[0]
 
+    # aux es el ultimo elemento de la lista
     aux = int(h[tam-1])
-    for i in range(tam-1, 0,-1):
+    for i in range(tam-1, 0, -1):
+        # aux2 pasa a ser el penultimo elemento de la list
         aux2 = int(h[i-1])
+        # se guarda el valor de aux en el penultimo lugar
         h[i-1] = aux
+        # y aux pasara a ser aux 2
         aux = int(aux2)
-        
-    min_heapify(h[:-1], 0)
-    return root, h[:-1]
 
-#II-B
+        # de esta manera en la siguiente iteracion aux sera el
+        # elemento que va a ser sustituido
+
+    # create min heap para que pase a ser heap
+    # y h[:-1] para reducir el tamaño de la array
+
+    return root, create_min_heap(h[:-1])
+
+# II-B
+
+
 def pq_ini():
+    # se crea una array de numpy de tipo 0 y se devuelve
     q = np.ndarray(shape=0)
     return q
 
-def pq_insert(h: np.ndarray, k: int)-> np.ndarray:
+
+def pq_insert(h: np.ndarray, k: int) -> np.ndarray:
+    # se inserta llamando a insert_min_heap
     return insert_min_heap(h, k)
 
-def pq_remove(h: np.ndarray)-> Tuple[int, np.ndarray]:
-    root,h = minheap_extract(h)
-    return [root, create_min_heap(h)]
+
+def pq_remove(h: np.ndarray) -> Tuple[int, np.ndarray]:
+    # se elimina llamando a min_heap_extract
+    return min_heap_extract(h)
 
 
-def min_heap_sort(h: np.ndarray)-> np.ndarray:
+def min_heap_sort(h: np.ndarray) -> np.ndarray:
+
+    # se crea un min heap y una lista de retorno
     h = create_min_heap(h)
     ret = list()
-    
-    while len(h) > 0:   
-        root,h = minheap_extract(h)
+
+    # mientras la longitud sea mayor que 0
+    while len(h) > 0:
+        # se va extrayendo la raiz (numero mas pequeño)
+        root, h = min_heap_extract(h)
+        # y se va añadiendo a la lista de retorno
         ret.append(root)
-        h = create_min_heap(h)
-    
+
     return np.array(ret)
--- p105/tiempos.py
+++ p105/tiempos.py
@@ -4,69 +4,74 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
+import math
 
-# Define tu función teórica f(N)
+# Funcion de coste teórico multiplicación de matríces
 def f(N):
-    # Aquí debes implementar tu función teórica
-    # Por ejemplo, podrías usar una función cuadrática
-    return 2 * N**3 + 3 * N + 1
+    return N**3
 
-# Define la función para ajustar los tiempos reales a la función teórica
+# Funcion de coste teorico short_min_heap
+def f_heap(N):
+    return N*(math.log(N, 10))
+
+# Funcion para ajustar los tiempos reales a la función teórica
+def tofit_heap(x, a, b):
+    return a * f_heap(x) + b
+
+# Funcion para ajustar los tiempos reales a la función teórica
 def tofit(x, a, b):
     return a * f(x) + b
 
-def graficasDot():
-    # Supongamos que tienes un array de tiempos reales a_timings
-    with open('./data/array.exe_data.json', 'r') as file:
+def graficasMult():
+    # Se abre el archivo
+    with open('./data/exe_data.json', 'r') as file:
         data = json.load(file)
-
+    # se extrae la array de datos del diccionario
     array_mult = np.array(data['array_mult'])
-
-    # Extraer los valores de N y los tiempos de ejecución
-
+    # y se extraen los valores de N y los tiempos de ejecución
     x = array_mult[: , 0]
-    #o (mas "pythonico"?) x = a_timings.T[0].T
     y = array_mult[: , 1]
-    
-    # Normaliza los tiempos dividiendo por el primer valor (opcional)
+
+    # se normalizan los tiempos 
     y = y / y[0]
 
-    # Ajusta los datos a la función tofit usando curve_fit
+    # para después ajustar los datos 
     pars, _ = curve_fit(tofit, x, y)
-
-    # Recuperar los parámetros ajustados (a y b)
     a, b = pars
 
-    # Generar valores ajustados usando la función teórica
+    # y guardar los valores de y que corresponderan con la función teórica
     y_fit = a * f(x) + b
 
-    # Graficar los tiempos reales y el ajuste calculado por la función
+    # Se crea la figura
     plt.figure(figsize=(10, 5))
-    plt.plot(x, y, marker='o', label='Tiempos reales')
-    plt.plot(x, y_fit, label='Ajuste')
+    # Se plotean los tiempos reales
+    plt.plot(x, y, marker='o', linestyle=' ', label='Tiempos reales')
+    # y los ajustados
+    plt.plot(x, y_fit, label='Ajuste: N^3')
+    # labels sobre los los ejes de la gráfica
     plt.xlabel('Tamaño de entrada (N)')
     plt.ylabel('Tiempo de Ejecución Normalizado')
     plt.title('Ajuste de Tiempos de Ejecución')
     plt.legend()
     plt.grid(True)
-
-    # Guardar la primera gráfica en un archivo PNG
+    # Se guarda la imagen
     plt.savefig('ej1.png')
     plt.close()
 
-def graficasMult():
-    # Cargar los datos desde el archivo JSON
-    with open('./data/array.exe_data.json', 'r') as file:
+def graficasMultDot():
+    # Se abre el archivo
+    with open('./data/exe_data.json', 'r') as file:
         data = json.load(file)
 
+    # se extraen las arrays de datos del diccionario
     array_mult = np.array(data['array_mult'])
     array_dot = np.array(data['array_dot'])
 
-    # Extraer los valores de N y los tiempos de ejecución
+    # y se extraen los valores de N y los tiempos de ejecución
     N_mult, tiempos_mult = array_mult[:, 0], array_mult[:, 1]
     N_dot, tiempos_dot = array_dot[:, 0], array_dot[:, 1]
 
-    # Crear la primera gráfica para array_mult
+    # A grandes rasgos esta parte es identica a la de la funcion anterior
     plt.figure(figsize=(10, 5))
     plt.plot(N_mult, tiempos_mult, marker='o', label='multiplication')
     plt.xlabel('N')
@@ -74,51 +79,46 @@ def graficasMult():
     plt.title('Tiempo matrix_multiplication() ')
     plt.legend()
     plt.grid(True)
-
-    # Guardar la primera gráfica en un archivo PNG
     plt.savefig('grafica_array_mult.png')
     plt.close()
 
-    # Crear la segunda gráfica para array_dot
+    # Segunda grafica pero con dot
     plt.figure(figsize=(10, 5))
     plt.plot(N_dot, tiempos_dot, marker='o', label='numpy', color='orange')
     plt.xlabel('N')
     plt.ylabel('Tiempo de Ejecución (s)')
-    plt.title('Tiempo matrix_multiplication() numpy')
+    plt.title('Tiempo numpy.ndarray.dot() ')
     plt.legend()
     plt.grid(True)
-
-    # Guardar la segunda gráfica en un archivo PNG
     plt.savefig('grafica_array_dot.png')
     plt.close()
 
 
 def graficasBB():
-    # Cargar los datos desde el archivo JSON
+    # Se abre el archivo
     with open('./data/exe_data.json', 'r') as file:
         data = json.load(file)
 
+    # se extraen las arrays de datos del diccionario
     bb_iter = np.array(data['bb_iter'])
     bb_rec = np.array(data['bb_rec'])
 
-    # Extraer los valores de N y los tiempos de ejecución
+    # y se extraen los valores de N y los tiempos de ejecución
     N_bbi, tiempos_bbi = bb_iter[:, 0], bb_iter[:, 1]
     N_bbrec, tiempos_bbrec = bb_rec[:, 0], bb_rec[:, 1]
 
-    # Crear la primera gráfica para array_mult
+    # Grafica de la busqueda binaria iterativa
     plt.figure(figsize=(10, 5))
     plt.plot(N_bbi, tiempos_bbi, marker='o', label='bbisearch')
     plt.xlabel('N')
     plt.ylabel('Tiempo de Ejecución (s)')
-    plt.title('Tiempo bb() ')
+    plt.title('Tiempo bb() iterativa ')
     plt.legend()
     plt.grid(True)
-
-    # Guardar la primera gráfica en un archivo PNG
     plt.savefig('grafica_bbi.png')
     plt.close()
 
-       # Crear la primera gráfica para array_mult
+    # Grafica de la busqueda binaria recursiva
     plt.figure(figsize=(10, 5))
     plt.plot(N_bbrec, tiempos_bbrec, marker='o', label='bbrecsearch')
     plt.xlabel('N')
@@ -126,39 +126,36 @@ def graficasBB():
     plt.title('Tiempo bb_rec() ')
     plt.legend()
     plt.grid(True)
-
-    # Guardar la primera gráfica en un archivo PNG
     plt.savefig('grafica_bb_rec.png')
     plt.close()
 
 
-    # Crear la primera gráfica para array_mult
+    # Grafica de las dos busquedas
     plt.figure(figsize=(10, 5))
-    plt.plot(N_bbi, tiempos_bbi, marker='o', label='bbisearch')
-    plt.plot(N_bbrec, tiempos_bbrec, marker='o', label='bbrecsearch')
+    plt.plot(N_bbi, tiempos_bbi, marker='o', label='bbi')
+    plt.plot(N_bbrec, tiempos_bbrec, marker='o', label='bbrec')
     plt.xlabel('N')
     plt.ylabel('Tiempo de Ejecución (s)')
     plt.title('Tiempo bbi() & bb_rec() ')
     plt.legend()
     plt.grid(True)
-
-    # Guardar la primera gráfica en un archivo PNG
     plt.savefig('grafica_bbi_rec.png')
     plt.close()
 
 
 
 def graficasCreateHeap():
-    # Cargar los datos desde el archivo JSON
+    # Se abre el archivo
     with open('./data/exe_data.json', 'r') as file:
         data = json.load(file)
 
+    # se extraen las arrays de datos del diccionario
     timings_minh = np.array(data['l_timings_min'])
 
-    # Extraer los valores de N y los tiempos de ejecución
+    # y se extraen los valores de N y los tiempos de ejecución
     N_minh, tiempos_createminh = timings_minh[:, 0], timings_minh[:, 1]
 
-    # Crear la primera gráfica para array_mult
+    # Grafica de la creacion de min heaps
     plt.figure(figsize=(10, 5))
     plt.plot(N_minh, tiempos_createminh, marker='o', label='createMinheap')
     plt.xlabel('N')
@@ -166,39 +163,52 @@ def graficasCreateHeap():
     plt.title('Tiempo create_min_heap() ')
     plt.legend()
     plt.grid(True)
-
-    # Guardar la primera gráfica en un archivo PNG
-    plt.savefig('grafica_minheap.png')
+    plt.savefig('grafica_createminheap.png')
     plt.close()
 
 
 def graficasShortHeap():
-    # Cargar los datos desde el archivo JSON
+    # Se abre el archivo
     with open('./data/exe_data.json', 'r') as file:
         data = json.load(file)
 
+    # se extraen las arrays de datos del diccionario
     l_timings_minsh = np.array(data['l_timings_minsh'])
 
-    # Extraer los valores de N y los tiempos de ejecución
+    # y se extraen los valores de N y los tiempos de ejecución
     N_minhsh, tiempos_minhsh = l_timings_minsh[:, 0], l_timings_minsh[:, 1]
 
-    # Crear la primera gráfica para array_mult
+    x = N_minhsh
+    y = tiempos_minhsh
+
+    # se normalizan los tiempos 
+    y = y / y[0]
+
+    # para después ajustar los datos 
+    pars, _ = curve_fit(tofit_heap, x, y)
+    a, b = pars
+
+    # y guardar los valores de y que corresponderan con la función teórica
+    y_fit = a * f(x) + b
+
+
+    # Grafica del short minheap
     plt.figure(figsize=(10, 5))
-    plt.plot(N_minhsh, tiempos_minhsh, marker='o', label='shortMinheap')
+    plt.plot(N_minhsh, tiempos_minhsh, marker=' ', label='shortMinheap')
+    plt.plot(x, y_fit, label='Ajuste: N log(N)')
     plt.xlabel('N')
     plt.ylabel('Tiempo de Ejecución (s)')
-    plt.title('Tiempo min_heap_sort() ')
+    plt.title('Tiempo min_heap_sort() ajustado')
     plt.legend()
     plt.grid(True)
-
-    # Guardar la primera gráfica en un archivo PNG
     plt.savefig('grafica_shortheap.png')
     plt.close()
 
 
 if __name__ == "__main__":
+    # llamada a las funciones que van creando las graficas
     graficasMult()
-    graficasDot()
+    graficasMultDot()
     graficasBB()
     graficasCreateHeap()
     graficasShortHeap()
