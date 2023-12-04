aded10beb6856db73388a0991a8349a1f6e494e3
Sergio <sergiohg818@gmail.com>
2023-12-04 17:24:17 +0100

    arreglo qs

--- p305/p305.py
+++ p305/p305.py
@@ -18,6 +18,7 @@ def split(t: np.ndarray) -> Tuple[np.ndarray, int, np.ndarray]:
     """
 
     mid = t[0]
+    # tabla de los menores y tabla de los mayores con list comprehension
     t_smallers = [u for u in t if u < mid]
     t_greaters = [u for u in t if u > mid]
     return t_smallers, mid, t_greaters
@@ -29,7 +30,7 @@ def qsel(t: np.ndarray, k: int) -> Union[int, None]:
         usando split.
     Args:
         t: array con los elementos.
-        k: posicion del elemento a encontrar.
+        k: posicion del elemento a encontrar (de 0 a len(t).
     Returns:
         Union: De el valor de k si existe y None si no.
     """
@@ -40,6 +41,10 @@ def qsel(t: np.ndarray, k: int) -> Union[int, None]:
 
     if len(t) == 1 and k == 0:
         return t[0]
+    
+    # si la lista es menor que CUTOFF se usa np.sort
+    if len(t) <= CUTOFF:
+        return np.sort(t)[k]
 
     t_smallers, mid, t_greaters = split(t)
     m = len(t_smallers)
@@ -58,26 +63,36 @@ def qsel_nr(t: np.ndarray, k: int) -> Union[int, None]:
         usando split.
     Args:
         t: array con los elementos.
-        k: posicion del elemento a encontrar.
+        k: posicion del elemento a encontrar (de 0 a len(t).
     Returns:
         Union: De el valor de k si existe y None si no.
     """
 
     # control de errores
-    if k >= len(t):
+    if k >= len(t) or k<0:
         return None
 
     aux_t = t.copy()
 
     # mientras aux_t tenga mas de un elemento y no sea el primero
     while len(aux_t) > 1 or k > 0:
+
+        # si la lista es menor que CUTOFF se usa np.sort
+        if len(t) <= CUTOFF:
+            return np.sort(t)[k]
+        
         t_smallers, mid, t_greaters = split(aux_t)
         m = len(t_smallers)
+
+        # si k es len(t_smallers)
         if k == m:
+            # el numero en la mitad es el buscado
             return mid
         elif k < m:
+            # si es menor esta en los pequeños
             aux_t = t_smallers
         else:
+            # si es mayor esta en los mayores
             k = k-m-1
             aux_t = t_greaters
 
@@ -99,6 +114,7 @@ def split_pivot(t: np.ndarray, mid: int) -> Tuple[np.ndarray, int, np.ndarray]:
     Returns:
         Tuple: Una tupla de los elementos menores, el elemento y los mayores.
     """
+    # tabla de los menores y tabla de los mayores con list comprehension
     t_smallers = [u for u in t if u < mid]
     t_greaters = [u for u in t if u > mid]
     return t_smallers, mid, t_greaters
@@ -114,20 +130,26 @@ def pivot5(t: np.ndarray) -> int:
         pivot: el pivote.
     """
 
+    # division para comprobar en cuantos grupos se parte
+    # y si hay algun grupo que no alcanza el groupsize
     n, remain = divmod(len(t), GROUPSIZE)
     
-    k = int(ceil(GROUPSIZE/2)) 
+    # posicion mediana del array
+    k = int(ceil(GROUPSIZE/2)) -1
 
-    medians = [np.sort(t[j * GROUPSIZE : (j+1) * GROUPSIZE])[k-1] for j in range(0, n)]
+    medians = [np.sort(t[j * GROUPSIZE : (j+1) * GROUPSIZE])[k] for j in range(0, n)]
     
     if remain:
-        k = int(ceil(remain/2))
-        medians.append(np.sort(t[-remain: ])[k-1])
+        k = int(ceil(remain/2)) -1
+        medians.append(np.sort(t[-remain: ])[k])
     
     
-    k = int(ceil(len(medians)/2))    
-    if len(medians) <= CUTOFF:            # if tabla_size <= cutff, sort and return k-position
-        pivot = np.sort(medians)[k - 1] 
+    # posicion mediana de las medianas
+    k = int(ceil(len(medians)/2)) - 1   
+    
+    # si la lista es menor que CUTOFF se usa np.sort
+    if len(medians) <= CUTOFF:
+        pivot = np.sort(medians)[k] 
     else:
         pivot = qsel5_nr(medians, k)
 
@@ -139,31 +161,36 @@ def qsel5_nr(t: np.ndarray, k: int)-> Union[int, None]:
         usando split.
     Args:
         t: array con los elementos.
-        k: posicion del elemento a encontrar.
+        k: posicion del elemento a encontrar (de 0 a len(t)).
     Returns:
         Union: De el valor de k si existe y None si no.
     """
 
-    if k >= len(t):
+    # control de errores
+    if k >= len(t) or k<0:
         return None
 
-    if len(t) == 1 and k == 0:
-        return t[0]
-    
+    aux_t = t.copy()
+    while len(aux_t) > 1 or k > 0:
+        
+        # si la lista es menor que CUTOFF se usa np.sort
+        if len(aux_t) <= CUTOFF:
+            return np.sort(aux_t)[k]
 
-    if len(t) <= CUTOFF:
-        return np.sort(t)[k-1]
-    
-    mid = pivot5(t)
-    s_1, pivot_value, s_2 = split_pivot(t, mid)
-    m = len(s_1)
-    
+        # busqueda del pivote y particion de la lista
+        mid = pivot5(aux_t)
+        t_smallers, pivot_value, t_greaters = split_pivot(aux_t, mid)
+        
+        # misma logica que el quick select tradicional
+        m = len(t_smallers)
+        if k == m:
+            return pivot_value
+        
+        elif k < m:
+            aux_t = t_smallers
+        else:
+            k = k - m-1 
+            aux_t = t_greaters
 
-    if k == m +1:
-        return pivot_value
-    
-    elif k < m +1:
-        return qsel5_nr(s_1, k)
-    else:
-        k = k - m-1 
-        return qsel5_nr(s_2, k)
+
+    return aux_t[0]
--- p305/test.py
+++ p305/test.py
@@ -1,14 +1,18 @@
 import numpy as np
 from p305 import qsel, qsel_nr, qsel5_nr
+from math import ceil
 
 l = [15, 3, 7, 2, 12, 9, 1, 6, 14, 11, 4, 8, 13, 5, 10 ] 
 ls = l.copy()
 ls.sort()
-k=0
-print(ls[k], ls)
 
 l2 = np.array(l)
 
-a = qsel5_nr(l2, k)
-print(a)
-print(l2)
+for i in range(len(l)):
+    
+    a = qsel5_nr(l2, i)
+    print(ls[i], end="")
+    print(", " + str(a))
+    print(" ")
+    print("--------------------------------- ")
+    print(" ")
